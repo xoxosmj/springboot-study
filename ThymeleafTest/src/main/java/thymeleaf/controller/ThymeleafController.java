cd7ef8bcc2bd1fbce463cb3dd3c23c636d31802a
semook <xoxosmj@gmail.com>
2024-11-06 11:44:08 +0900

    타임리푸 2일차

--- ThymeleafTest/src/main/java/thymeleaf/controller/ThymeleafController.java
+++ ThymeleafTest/src/main/java/thymeleaf/controller/ThymeleafController.java
@@ -2,6 +2,7 @@ package thymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import thymeleaf.bean.PersonDTO;
 
@@ -53,6 +54,41 @@ public class ThymeleafController {
 
         return "basic/operation";
 
+    }
+
+    @GetMapping(value = "text_basic")
+    public String text_basic(ModelMap modelMap) {
+        modelMap.put("data", "머시기머시기");
+        return "basic/text_basic";
+    }
+
+    @GetMapping(value = "attribute")
+    public String attribute(Model model) {
+        model.addAttribute("data", "머시기머시기");
+        return "basic/attribute";
+    }
+
+    @GetMapping(value = "ifcondition")
+    public String ifcondition(Model model) {
+        model.addAttribute("age", 25);
+        model.addAttribute("movie", "베놈");
+        model.addAttribute("today", "수요일");
+        return "basic/ifcondition";
+    }
+
+    @GetMapping(value = "condition")
+    public String condition(Model model) {
+        PersonDTO aa = new PersonDTO("홍길동", 25);
+        PersonDTO bb = new PersonDTO("김태리", 17);
+        PersonDTO cc = new PersonDTO("이제훈", 40);
+
+        List<PersonDTO> list = new ArrayList<>();
+        list.add(aa);
+        list.add(bb);
+        list.add(cc);
+
+        model.addAttribute("list", list);
+        return "basic/condition";
     }
 }
 
