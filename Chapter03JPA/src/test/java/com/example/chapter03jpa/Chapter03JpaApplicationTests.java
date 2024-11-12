d98ffd98111934ec7ba7a70f154fcfa11be91aad
semook <xoxosmj@gmail.com>
2024-11-12 16:30:50 +0900

    jpa test

---
+++ Chapter03JPA/src/test/java/com/example/chapter03jpa/Chapter03JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chapter03jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter03JpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
