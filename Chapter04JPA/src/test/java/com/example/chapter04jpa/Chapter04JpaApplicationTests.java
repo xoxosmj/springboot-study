fbfe5e6744229c0e02837c6f832424c17cf28579
semook <xoxosmj@gmail.com>
2024-11-13 16:33:39 +0900

    chapter04 init

---
+++ Chapter04JPA/src/test/java/com/example/chapter04jpa/Chapter04JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chapter04jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter04JpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
